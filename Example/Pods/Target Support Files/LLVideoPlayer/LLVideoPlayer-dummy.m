a4d2929f40397314ba88fee4e1187d3657bc8a9c
mario <mohu3g@163.com>
2017-03-01 15:43:26 +0800

    rm Pods, lock

--- Example/Pods/Target Support Files/LLVideoPlayer/LLVideoPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LLVideoPlayer : NSObject
-@end
-@implementation PodsDummy_LLVideoPlayer
-@end
--- Example/Pods/Target Support Files/Pods-LLVideoPlayer_Example/Pods-LLVideoPlayer_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LLVideoPlayer_Example : NSObject
-@end
-@implementation PodsDummy_Pods_LLVideoPlayer_Example
-@end
